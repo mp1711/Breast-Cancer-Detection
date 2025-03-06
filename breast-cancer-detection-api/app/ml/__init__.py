0ac8cd89b9e822b71d4fb63e147902f4f608f1f6
mp1711 <136905595+mp1711@users.noreply.github.com>
2025-03-07 00:40:20 +0530

    Delete breast-cancer-detection-api directory

--- breast-cancer-detection-api/app/ml/__init__.py
+++
@@ -1,3 +0,0 @@
-# /skin-cancer-detection-app/skin-cancer-detection-app/backend/app/ml/__init__.py
-
-# This file is intentionally left blank.
--- breast-cancer-detection-api/app/storage/__init__.py
+++
@@ -1,3 +0,0 @@
-# File: /skin-cancer-detection-app/skin-cancer-detection-app/backend/app/storage/__init__.py
-
-# This file is intentionally left blank.
